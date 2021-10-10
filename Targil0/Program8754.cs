23926190917307993b98fb1b7cab3fee7cdaaef8
hodayaAsh <hoashken@g.jct.ac.il>
2021-10-10 14:24:24 +0300

    Initial code - Welcome functions

---
+++ Targil0/Program8754.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome8754();
+            Welcome2775();
+            Console.ReadKey();
+        }
+        static partial void Welcome2775();
+        private static void welcome8754()
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
