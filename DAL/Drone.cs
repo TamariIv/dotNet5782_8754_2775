0e9ef12cd62cbf12459352ef60ab531b3370718b
TamariIv <taivgi@g.jct.ac.il>
2021-10-18 12:48:01 +0300

    initializing drone, droneCharge and station classes

---
+++ DAL/Drone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            //public WeightCategories MaxWeight;
+            //public DroneStatus Status;
+            public double Battery { get; set; }
+        }
+    }
+
+}
---
+++ DAL/Station.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Station
+        {
+            public int Id;
+            public int Name;
+            public double Lonitude;
+            public double Lattitude;
+            public int CahrgeSlots;
+        }
+    }
+
+}
