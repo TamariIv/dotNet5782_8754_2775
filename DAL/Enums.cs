881b9c18851a78552dc8687bd7699b46e65a6979
TamariIv <taivgi@g.jct.ac.il>
2021-10-18 13:00:23 +0300

    add enums file

---
+++ DAL/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        enum WeightCategories { heavy, medium, light}
+    }
+    class Enums
+    {
+    }
+}
