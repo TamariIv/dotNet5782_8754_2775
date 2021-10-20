6c3e53e7aadf50444da2e287258aaebb8599bd6b
TamariIv <taivgi@g.jct.ac.il>
2021-10-20 17:23:33 +0300

    add config class

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.DO;
 
 namespace DAL
 {
@@ -11,6 +12,15 @@ namespace DAL
         class DataSource
         {
 
+        }
+
+        internal class Config
+        {
+            internal static int Drone_i = 0;
+            internal static int Station_i = 0;
+            internal static int Customer_i = 0;
+            internal static int Parcel_i = 0;
+            internal static int ParcelNum = 0;
         }
     }
 }
