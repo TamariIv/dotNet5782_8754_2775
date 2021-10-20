c37d8985152019dee1c070a0ed752493e4883715
TamariIv <taivgi@g.jct.ac.il>
2021-10-20 17:39:12 +0300

    add lists

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -11,16 +11,19 @@ namespace DAL
     {
         class DataSource
         {
-            internal Drone
+            internal List<Object> Drones;
+            internal List<Object> Station;
+            internal List<Object> Customers;
+            internal List<Object> Parcels;
         }
 
         internal class Config
         {
-            internal static int Drone_i = 0;
-            internal static int Station_i = 0;
-            internal static int Customer_i = 0;
-            internal static int Parcel_i = 0;
-            internal static int ParcelNum = 0;
+            //internal static int Drone_i = 0;
+            //internal static int Station_i = 0;
+            //internal static int Customer_i = 0;
+            //internal static int Parcel_i = 0;
+            //internal static int ParcelNum = 0;
         }
     }
 }
