549a2a859a5ba4cce4ad4d9e55743b0afbea7bff
hodayaAsh <hoashken@g.jct.ac.il>
2021-10-20 19:19:41 +0300

    Initializing station in initialize function

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -11,10 +11,13 @@ namespace DAL
     {
         public class DataSource
         {
-            internal List<Object> Drones;
-            internal List<Object> Station;
-            internal List<Object> Customers;
-            internal List<Object> Parcels;
+            internal List<Drone> Drones;
+            internal static List<Station> Stations;
+            internal List<Customer> Customers;
+            internal List<Parcel> Parcels;
+
+
+            public static Random r = new Random();
 
             internal class Config
             {
@@ -24,10 +27,21 @@ namespace DAL
                 //internal static int Parcel_i = 0;
                 //internal static int ParcelNum = 0;
                 internal int ParcelId;
+
             }
             public static void Initialize()
             {
-
+                for (int i = 0; i < 2; i++) //initialize station
+                {
+                    Station s = new Station();
+                    s.Id = r.Next(10000, 10000);
+                    s.Name = r.Next(10000, 100000);
+                    s.Lonitude = r.NextDouble() + r.Next(-180, 80);
+                    s.Lattitude = r.NextDouble() + r.Next(-90, 90);
+                    s.CahrgeSlots = r.Next(1, 6);
+                    Stations.Add(s);
+                }
+               
             }
         }
 
