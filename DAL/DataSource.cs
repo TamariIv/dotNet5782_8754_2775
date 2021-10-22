87485bccc30b96afaf2851bf16143ac420c76d9c
hodayaAsh <hoashken@g.jct.ac.il>
2021-10-22 14:10:35 +0300

    initialize parcel

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -22,17 +22,19 @@ namespace DAL
             {        
                 internal int ParcelId = 1000000; ///the first parcel num. (and than it rises in one)
             }
-
+            /// <summary>
+            /// initialize the lists 
+            /// </summary>
             public static void Initialize()
-            {   ///initialize the list of the stations:
+            {   //initialize stations
                 Station s = new Station();
-                s.Id = r.Next(10000, 100000); ///id number with 5 digits
+                s.Id = r.Next(10000, 100000); //id number with 5 digits
                 s.Name = "Pisgat Zeev";
                 s.Longitude = 31.831146861430444;
                 s.Latitude = 35.24263233548004;
                 s.CahrgeSlots = r.Next(11);
                 Stations.Add(s);
-
+            
                 s = new Station();
                 s.Id = r.Next(10000, 100000);
                 s.Name = "Givat Shaul";
@@ -43,9 +45,20 @@ namespace DAL
 
                 for (int i = 0; i < 10; i++)
                 {
-                    Parcel p = new Parcel();
-                    p.Id =
 
+                    Parcel p = new Parcel();
+                    p.Id = r.Next(100000, 1000000);
+                    p.SenderId = r.Next(10000000, 100000000); //buisness number
+                    int temp = r.Next(0, Customers.Count()); //random number between 0 - length of the customers list as index for TargetId
+                    p.TargetId = Customers.ElementAt(temp).Id; //the ID of the cutomer
+                    p.Weight = (WeightCategories)r.Next(3);
+                    p.Priority = (Priorities)r.Next(3);
+                    //DateTime start = new DateTime(2020, 1, 1);
+                    //int range = (DateTime.Today - start).Days;
+                    //p.PickedUp = start.AddDays(r.Next(range));
+                    //start = DateTime.Today;
+                    //p.Delivered = (p.PickedUp - (DateTime)r.Next(15));
+                    p.DroneId
                 }
 
                 for (int i = 0; i < 5; i++) //initialize drone
@@ -57,7 +70,7 @@ namespace DAL
                     Drones.Add(d);
                 }
 
-
+                //initialize customer
                 string[] namesArray = { "Avraham", "Yitshak", "Yaakov", "Sarah", "Rivka", "Rahel", "Leah", "David", "Moshe", "Aharon" };
                 string[] firstDigits = { "050", "052", "054" };
                 for (int i = 0; i < 5; i++)
@@ -65,13 +78,13 @@ namespace DAL
                     Customer c = new Customer();
                     c.Id = r.Next(100000000, 1000000000);
                     c.Name = namesArray[i];
-                    c.Phone = firstDigits[r.Next(3)] + r.Next(1111111, 10000000);
+                    c.Phone = firstDigits[r.Next(3)];
                     for (int j = 0; j < 7; j++)
                     {
                         c.Phone += (r.Next(0, 11)).ToString();
                     }
-                    c.Latitude = r.NextDouble() + r.Next(-90, 90);
-                    c.Longitude = r.NextDouble() + r.Next(-180, 80);
+                    c.Latitude = 35 + r.NextDouble(); 
+                    c.Longitude = 31 + r.NextDouble();
                 }
 
 
