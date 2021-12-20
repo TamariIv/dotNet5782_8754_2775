c5e7240b25b718c2f8858793d7ff7a40c0a0756e
TamariIv <taivgi@g.jct.ac.il>
2021-12-20 23:37:34 +0200

    c

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using IBL;
+using BlApi;
 
 namespace PL
 {
@@ -21,10 +21,10 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
-        IBL.IBL mybl;
+        IBL mybl;
         public MainWindow()
         {
-            mybl = new BL.BlObject();
+            mybl = BlFactory.GetBl();
             InitializeComponent();
         }
 
